53e005b7e88d8c7cbaf919576ec25a3838dd868c
Martin Metodiev <martin.metodiew@gmail.com>
2024-11-25 17:53:10 +0200

    theme: add basic static footer

--- wp-content/themes/brandit/footer.php
+++ wp-content/themes/brandit/footer.php
@@ -1,5 +1,60 @@
-<footer>
-    <p>This is the footer</p>
+<footer class="main-footer-section">
+    <div class="container-xxl">
+        <div class="row justify-content-between pt-5 pb-5">
+            <div class="footer-column col-xl-4 col-lg-4 mb-50 ml-50">
+                <div class="footer-logo">
+                    <a href="<?php echo esc_url( site_url( '/' ) ); ?>">Brand It</a>
+                </div>
+                <div class="">
+                    <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor
+                        incididuntut consec tetur adipisicing
+                        elit,Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="footer-social-icons">
+                    <h5>Follow us</h5>
+                    <a href="#">icon 1</a>
+                    <a href="#">icon 2</a>
+                    <a href="#">icon 3</a>
+                </div>
+            </div>
+            <div class="footer-column col-xl-2 col-lg-2 col-md-6 mb-30">
+                <div class="footer-section-heading">
+                    <h3>Services</h3>
+                </div>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Service 1</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Service 2</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Service 3</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Service 4</a></li>
+                </ul>
+            </div>
+            <div class="footer-column col-xl-2 col-lg-2 col-md-6 mb-30">
+                <div class="footer-section-heading">
+                    <h3>Useful Links</h3>
+                </div>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Home</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">About</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Contacts</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="footer-column col-xl-3 col-lg-3 col-md-6 mb-50">
+                <div class="footer-section-heading">
+                    <h3>Contacts</h3>
+                </div>
+                <div class="mb-25">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright-area">
+        <div class="container-xxl text-center">
+            <p class="copyright-text">Copyright &copy; <?php echo date( "Y" ); ?> Brand It. All Rights Reserved</p>
+        </div>
+    </div>
+
 </footer>
 
 <?php wp_footer(); ?>

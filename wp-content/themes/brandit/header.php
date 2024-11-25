72bb7b88a5a20d5dbb03454dc728009a9e235b4e
Martin Metodiev <martin.metodiew@gmail.com>
2024-11-25 16:45:45 +0200

    theme: add basic static header

--- wp-content/themes/brandit/header.php
+++ wp-content/themes/brandit/header.php
@@ -12,6 +12,39 @@
 
 <?php wp_body_open(); ?>
 
-<header>
-    This is the header...
+<header class="">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-xxl">
+            <a href="<?php echo esc_url( site_url( '/' ) ); ?>" class="navbar-brand">
+                <span class="fs-4">Brand It</span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
+                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul class="navbar-nav nav-underline mr-auto w-100 justify-content-end">
+                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                           aria-expanded="false">
+                            Services
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Service 1</a></li>
+                            <li><a class="dropdown-item" href="#">Service 2</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">See All Services</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Contacts</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>

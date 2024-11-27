39fe96a30f605b73a2f5a3a1f6106f993e84b5af
Martin Metodiev <martin.metodiew@gmail.com>
2024-11-27 16:12:52 +0200

    theme: make some footer UI improvements

--- wp-content/themes/brandit/footer.php
+++ wp-content/themes/brandit/footer.php
@@ -1,26 +1,26 @@
 <footer class="main-footer-section">
     <div class="container-xxl">
         <div class="row justify-content-between pt-5 pb-5">
-            <div class="footer-column col-xl-4 col-lg-4 mb-50 ml-50">
-                <div class="footer-logo">
-                    <a href="<?php echo esc_url( site_url( '/' ) ); ?>">Brand It</a>
-                </div>
-                <div class="">
-                    <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor
-                        incididuntut consec tetur adipisicing
-                        elit,Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div class="footer-social-icons">
-                    <h5>Follow us</h5>
-                    <a href="#">icon 1</a>
-                    <a href="#">icon 2</a>
-                    <a href="#">icon 3</a>
+            <div class="footer-column col-lg-3 col-md-6">
+                <div class="footer-item">
+                    <h2 class="mb-4">
+						<?php get_template_part( 'partials/logo' ); ?>
+                    </h2>
+                    <p class="lh-lg mb-4">
+                        There cursus massa at urnaaculis estieSed aliquamellus vitae ultrs condmentum leo massamollis
+                        its estiegittis miristum.
+                    </p>
+
+                    <div class="footer-icon d-flex pt-2">
+                        <a class="btn btn-outline-primary me-1" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-primary me-1" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-primary me-1" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-primary me-0" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
                 </div>
             </div>
             <div class="footer-column col-xl-2 col-lg-2 col-md-6 mb-30">
-                <div class="footer-section-heading">
-                    <h3>Services</h3>
-                </div>
+                <h3 class="footer-section-heading">Services</h3>
                 <ul class="nav flex-column">
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Service 1</a></li>
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Service 2</a></li>
@@ -29,9 +29,7 @@
                 </ul>
             </div>
             <div class="footer-column col-xl-2 col-lg-2 col-md-6 mb-30">
-                <div class="footer-section-heading">
-                    <h3>Useful Links</h3>
-                </div>
+                <h3 class="footer-section-heading">Useful Links</h3>
                 <ul class="nav flex-column">
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Home</a></li>
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0">About</a></li>
@@ -39,13 +37,24 @@
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Privacy</a></li>
                 </ul>
             </div>
-            <div class="footer-column col-xl-3 col-lg-3 col-md-6 mb-50">
-                <div class="footer-section-heading">
-                    <h3>Contacts</h3>
-                </div>
-                <div class="mb-25">
+            <div class="footer-column col-lg-3 col-md-6">
+                <div class="footer-item">
+                    <h3 class="footer-section-heading">Contact Us</h3>
+                    <div class="d-flex flex-column align-items-start">
+                        <p><i class="fa fa-map-marker-alt me-2"></i> 123 Street, New York, USA</p>
+                        <p><i class="fa fa-phone-alt me-2"></i> (+012) 3456 7890 123</p>
+                        <p><i class="fas fa-envelope me-2"></i> info@example.com</p>
+                        <p><i class="fa fa-clock me-2"></i> 26/7 Hours Service</p>
+                    </div>
                 </div>
             </div>
+            <!--            <div class="footer-column col-xl-3 col-lg-3 col-md-6 mb-50">-->
+            <!--                <div class="footer-section-heading">-->
+            <!--                    <h3>Contacts</h3>-->
+            <!--                </div>-->
+            <!--                <div class="mb-25">-->
+            <!--                </div>-->
+            <!--            </div>-->
         </div>
     </div>
 

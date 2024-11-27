d66b0fce1e5b9c4a15a45f2b95a6a5c2dbac0a87
Martin Metodiev <martin.metodiew@gmail.com>
2024-11-27 16:09:41 +0200

    theme: add logo and make some header UI improvements

--- wp-content/themes/brandit/header.php
+++ wp-content/themes/brandit/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 	<?php wp_head(); ?>
 </head>
 
@@ -12,20 +12,53 @@
 
 <?php wp_body_open(); ?>
 
-<header class="">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-xxl">
-            <a href="<?php echo esc_url( site_url( '/' ) ); ?>" class="navbar-brand">
-                <span class="fs-4">Brand It</span>
+<header class="header">
+    <!--    <nav class="navbar navbar-expand-lg bg-body-tertiary">-->
+    <!--        <div class="container-xxl">-->
+    <!--            <a href="--><?php ///*echo esc_url( site_url( '/' ) );*/ ?><!--" class="navbar-brand">-->
+    <!--                <span class="fs-4">Brand It</span>-->
+    <!--            </a>-->
+    <!---->
+    <!--            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"-->
+    <!--                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">-->
+    <!--                <span class="navbar-toggler-icon"></span>-->
+    <!--            </button>-->
+    <!---->
+    <!--            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">-->
+    <!--                <ul class="navbar-nav nav-underline mr-auto w-100 justify-content-end">-->
+    <!--                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>-->
+    <!--                    <li class="nav-item dropdown">-->
+    <!--                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"-->
+    <!--                           aria-expanded="false">-->
+    <!--                            Services-->
+    <!--                        </a>-->
+    <!--                        <ul class="dropdown-menu">-->
+    <!--                            <li><a class="dropdown-item" href="#">Service 1</a></li>-->
+    <!--                            <li><a class="dropdown-item" href="#">Service 2</a></li>-->
+    <!--                            <li>-->
+    <!--                                <hr class="dropdown-divider">-->
+    <!--                            </li>-->
+    <!--                            <li><a class="dropdown-item" href="#">See All Services</a></li>-->
+    <!--                        </ul>-->
+    <!--                    </li>-->
+    <!--                    <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>-->
+    <!--                    <li class="nav-item"><a href="#" class="nav-link">Contacts</a></li>-->
+    <!--                </ul>-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </nav>-->
+    <div class="container-xxl">
+        <nav class="navbar navbar-light navbar-expand-lg">
+            <a href="index.html" class="navbar-brand">
+                <!--                <h1 class="text-primary fw-bold mb-0">Cater<span class="text-dark">Serv</span></h1>-->
+				<?php get_template_part( 'partials/logo' ); ?>
             </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
-                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse">
+                <span class="fa fa-bars text-primary"></span>
             </button>
-
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <ul class="navbar-nav nav-underline mr-auto w-100 justify-content-end">
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav nav-underline mx-auto">
                     <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
@@ -44,7 +77,10 @@
                     <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Contacts</a></li>
                 </ul>
+                <a href="" class="btn btn-outline-primary py-2 px-4 d-none d-xl-inline-block">Book Now</a>
+                <button class="btn-search btn btn-outline-primary btn-md-square ms-4 rounded-circle d-none d-lg-inline-flex"
+                        data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search"></i></button>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </header>

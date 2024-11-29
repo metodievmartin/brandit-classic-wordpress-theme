7d9d38e43b09b562036ce29695f5fbdaf74ea6ec
Martin Metodiev <martin.metodiew@gmail.com>
2024-11-29 16:14:15 +0200

    theme: create a contact page with a static form

--- wp-content/themes/brandit/header.php
+++ wp-content/themes/brandit/header.php
@@ -13,50 +13,18 @@
 <?php wp_body_open(); ?>
 
 <header class="header">
-    <!--    <nav class="navbar navbar-expand-lg bg-body-tertiary">-->
-    <!--        <div class="container-xxl">-->
-    <!--            <a href="--><?php ///*echo esc_url( site_url( '/' ) );*/ ?><!--" class="navbar-brand">-->
-    <!--                <span class="fs-4">Brand It</span>-->
-    <!--            </a>-->
-    <!---->
-    <!--            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"-->
-    <!--                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">-->
-    <!--                <span class="navbar-toggler-icon"></span>-->
-    <!--            </button>-->
-    <!---->
-    <!--            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">-->
-    <!--                <ul class="navbar-nav nav-underline mr-auto w-100 justify-content-end">-->
-    <!--                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>-->
-    <!--                    <li class="nav-item dropdown">-->
-    <!--                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"-->
-    <!--                           aria-expanded="false">-->
-    <!--                            Services-->
-    <!--                        </a>-->
-    <!--                        <ul class="dropdown-menu">-->
-    <!--                            <li><a class="dropdown-item" href="#">Service 1</a></li>-->
-    <!--                            <li><a class="dropdown-item" href="#">Service 2</a></li>-->
-    <!--                            <li>-->
-    <!--                                <hr class="dropdown-divider">-->
-    <!--                            </li>-->
-    <!--                            <li><a class="dropdown-item" href="#">See All Services</a></li>-->
-    <!--                        </ul>-->
-    <!--                    </li>-->
-    <!--                    <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>-->
-    <!--                    <li class="nav-item"><a href="#" class="nav-link">Contacts</a></li>-->
-    <!--                </ul>-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--    </nav>-->
     <div class="container-xxl">
         <nav class="navbar navbar-light navbar-expand-lg">
-            <a href="index.html" class="navbar-brand">
-                <!--                <h1 class="text-primary fw-bold mb-0">Cater<span class="text-dark">Serv</span></h1>-->
+
+            <a href="<?php echo esc_url( site_url( '/' ) ); ?>" class="navbar-brand">
 				<?php get_template_part( 'partials/logo' ); ?>
             </a>
+
             <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars text-primary"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav nav-underline mx-auto">
                     <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
@@ -75,12 +43,19 @@
                         </ul>
                     </li>
                     <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Contacts</a></li>
+                    <li class="nav-item">
+                        <a href="<?php echo esc_url( site_url( '/contacts' ) ); ?>" class="nav-link">Contacts</a>
+                    </li>
                 </ul>
+
                 <button class="search-button btn-md-square me-4 d-none d-lg-inline-flex"
-                        data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search"></i></button>
+                        data-bs-toggle="modal" data-bs-target="#searchModal">
+                    <i class="fas fa-search"></i>
+                </button>
+
                 <a href="" class="btn btn-outline-primary py-2 px-4 d-none d-xl-inline-block">Book Now</a>
             </div>
+
         </nav>
     </div>
 </header>
---
+++ wp-content/themes/brandit/page-contacts.php
@@ -0,0 +1,91 @@
+<?php get_header(); ?>
+
+<?php get_template_part( 'partials/page-banner' ); ?>
+
+    <section class="contacts-section container-xxl py-5 my-5">
+        <div class="row justify-content-center">
+
+            <div class="col-12 mb-5">
+                <div class="row row-cols-1 row-cols-lg-3 g-3">
+                    <div class="col">
+                        <div class="h-100 d-flex border border-primary p-4">
+                            <i class="fas fa-map-marker-alt fa-2x text-primary me-4"></i>
+                            <div>
+                                <h4>Address</h4>
+                                <p>123 Street, New York, USA</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="h-100 d-flex border border-primary p-4">
+                            <i class="fas fa-envelope fa-2x text-primary me-4"></i>
+                            <div>
+                                <h4>Mail Us</h4>
+                                <p class="mb-2">info@example.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="h-100 d-flex border border-primary p-4">
+                            <i class="fa fa-phone-alt fa-2x text-primary me-4"></i>
+                            <div>
+                                <h4>Telephone</h4>
+                                <p class="mb-2">(+012) 3456 7890 123</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-12 text-center mb-5">
+                <small class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Get
+                    in touch</small>
+                <h1 class="display-5 mb-0">Contact Us For Any Queries!</h1>
+            </div>
+
+            <div class="contact-form-container col-md-6 col-lg-7">
+                <form class="contact-form" data-np-autofill-form-type="identity" data-np-checked="1"
+                      data-np-watching="1">
+                    <div class="row g-3">
+                        <div class="col-md-6">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="name" placeholder="Your Name"
+                                       data-np-uid="6d5c9069-b4ae-411d-9aaa-4e6c24b7fa61"
+                                       data-np-autofill-field-type="fullName">
+                                <label for="name">Your Name</label>
+
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-floating">
+                                <input type="email" class="form-control" id="email" placeholder="Your Email"
+                                       data-np-uid="8220425d-5b07-4806-bc75-71b08c43f202"
+                                       data-np-autofill-field-type="email">
+                                <label for="email">Your Email</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="subject" placeholder="Subject"
+                                       data-np-intersection-state="visible">
+                                <label for="subject">Subject</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating">
+                                        <textarea class="form-control" placeholder="Leave a message here" id="message"
+                                                  style="height: 100px" data-np-intersection-state="visible"></textarea>
+                                <label for="message">Message</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-secondary w-100 py-3" type="submit">Send Message</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer(); ?>

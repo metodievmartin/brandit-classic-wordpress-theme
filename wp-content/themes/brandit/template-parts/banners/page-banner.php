e09920e7d488f8ab9f9143f7054e44d84be9f50c
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-04 22:30:26 +0200

    theme: refactor page-banner to receive a page title as argument

--- wp-content/themes/brandit/template-parts/banners/page-banner.php
+++ wp-content/themes/brandit/template-parts/banners/page-banner.php
@@ -1,5 +1,9 @@
+<?php
+$page_title = $args['page_title'] ?? get_the_title();
+?>
+
 <section class="main-page-banner container-fluid">
     <div class="container-xxl text-center">
-        <h1 class="display-1 mb-4"><?php the_title() ?></h1>
+        <h1 class="display-1 mb-4"><?php echo esc_html( $page_title ); ?></h1>
     </div>
 </section>

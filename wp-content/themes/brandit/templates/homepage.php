c1b53bbb61cbb890508e3ac0ddea44cca6f7f2ca
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-05 20:55:44 +0200

    theme: create a homepage template

---
+++ wp-content/themes/brandit/templates/homepage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Homepage
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="container-xxl">
+    <h1>Hello world!</h1>
+</div>
+
+<?php render_services_section(); ?>
+
+<?php get_footer(); ?>

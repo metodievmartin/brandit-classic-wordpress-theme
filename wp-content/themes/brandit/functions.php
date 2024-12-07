a7bfd59217645682d0bfaddf92ba04a75ce3d5aa
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-07 22:33:58 +0200

    theme: add a category archive page

--- wp-content/themes/brandit/functions.php
+++ wp-content/themes/brandit/functions.php
@@ -48,3 +48,13 @@ function brandit_add_support() {
 }
 
 add_action( 'after_setup_theme', 'brandit_add_support' );
+
+function remove_prefix_from_category_archive_page( $title ) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	}
+
+	return $title;
+}
+
+add_filter( 'get_the_archive_title', 'remove_prefix_from_category_archive_page' );

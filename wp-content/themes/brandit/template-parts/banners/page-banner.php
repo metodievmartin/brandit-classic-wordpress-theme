2ba1dd381b0ab23624af9ddcc6d9302bd1cee714
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-07 22:33:14 +0200

    theme: add a dynamic subtitle to page banner

--- wp-content/themes/brandit/template-parts/banners/page-banner.php
+++ wp-content/themes/brandit/template-parts/banners/page-banner.php
@@ -1,9 +1,15 @@
 <?php
-$page_title = $args['page_title'] ?? get_the_title();
+$page_title    = $args['page_title'] ?? get_the_title();
+$page_subtitle = $args['page_subtitle'] ?? '';
 ?>
 
 <section class="main-page-banner container-fluid">
     <div class="container-xxl text-center">
         <h1 class="display-1 mb-4"><?php echo esc_html( $page_title ); ?></h1>
+
+		<?php if ( ! empty( $page_subtitle ) ): ?>
+            <h4 class="banner-subtitle fw-light"><?php echo esc_html( $page_subtitle ); ?></h4>
+		<?php endif; ?>
+
     </div>
 </section>

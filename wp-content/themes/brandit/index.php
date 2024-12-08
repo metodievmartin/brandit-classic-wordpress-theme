35c581f9573e695bd03ecfd5ed285ccb9166bde8
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-08 18:34:55 +0200

    theme: refactor the default blog listing page using template parts

--- wp-content/themes/brandit/index.php
+++ wp-content/themes/brandit/index.php
@@ -1,68 +1,21 @@
 <?php get_header(); ?>
 
 <?php
-get_template_part(
-	'template-parts/banners/page-banner',
-	null,
-	array( 'page_title' => 'Welcome to our Blog' )
+$page_banner_args  = array(
+	'page_title' => __( 'Welcome to our Blog', 'brandit' ),
+);
+$blog_listing_args = array(
+	'section_top_title' => __( 'Blog', 'brandit' ),
 );
 ?>
 
-    <section class="blog-section page-section container-xxl">
+<main class="default-archive-page blog-listing-page">
 
-        <div class="container-fluid blog py-6">
-            <div class="container">
-                <div class="text-center">
-                    <h5 class="skewed-title">Blog</h5>
-                    <h1 class="display-5 mb-5">Keep up with our latest news.</h1>
-                </div>
-            </div>
-        </div>
+	<?php get_template_part( 'template-parts/banners/page-banner', null, $page_banner_args ); ?>
 
-        <div class="row g-5">
+	<?php get_template_part( 'template-parts/blog/blog-listing-grid', null, $blog_listing_args ); ?>
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-                    <div class="col-md-6 col-lg-4">
-
-                        <div class="blog-item h-100">
-                            <div class="overflow-hidden rounded image-container">
-                                <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid w-100"
-                                     alt="Blog post image">
-                            </div>
-
-							<?php
-							$blog_args = array(
-								'title'      => get_the_title(),
-								'permalink'  => get_the_permalink(),
-								'event_date' => get_the_date(),
-							);
-
-							get_template_part( 'template-parts/blog/blog-mini-item', null, $blog_args );
-
-							?>
-
-                            <div class="blog-item-content px-4">
-								<?php echo get_excerpt_or_first_n_words( 15 ) ?>
-                            </div>
-                        </div>
-
-                    </div>
-
-				<?php endwhile; ?>
-
-			<?php else : ?>
-                <p><?php esc_html__( 'Sorry, there are no posts yet' ); ?></p>
-			<?php endif; ?>
-
-        </div>
-
-        <div class="pagination-container text-center pt-3 my-4">
-			<?php echo paginate_links() ?>
-        </div>
-
-    </section>
+</main>
 
 <?php get_footer(); ?>
+

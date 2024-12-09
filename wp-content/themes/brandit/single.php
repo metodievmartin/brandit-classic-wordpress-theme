e48aef52aa59974ccaf1817a03ea7541bc8a8bb8
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-09 18:43:32 +0200

    theme: update markup structure and thumbnail display logic

--- wp-content/themes/brandit/single.php
+++ wp-content/themes/brandit/single.php
@@ -1,47 +1,52 @@
 <?php get_header(); ?>
 
-<?php
-get_template_part(
-	'template-parts/banners/page-banner',
-	null,
-	array( 'page_title' => get_the_title() )
-);
-?>
+    <main>
 
-    <section class="services-section page-section container-xxl">
-        <div class="row justify-content-center g-5">
+		<?php
+		get_template_part(
+			'template-parts/banners/page-banner',
+			null,
+			array( 'page_title' => get_the_title() )
+		);
+		?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+        <section class="services-section page-section container-xxl">
+            <div class="row justify-content-center g-5">
 
-                <div class="container container--narrow page-section position-relative">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-                    <div class="metabox metabox--position-up metabox--with-home-link">
-                        <p>
-                            <a class="metabox__blog-home-link" href="<?php echo esc_url( site_url( '/blog' ) ); ?>">
-                                <i class="fa fa-home" aria-hidden="true"></i> Blog Home
-                            </a>
-                            <span class="metabox__main">
-					Posted by <?php the_author_posts_link(); ?> on <?php the_time( 'd/M/Y' ); ?> in <?php echo get_the_category_list( ', ' ); ?>
-				</span>
-                        </p>
-                    </div>
+                    <div class="container container--narrow page-section position-relative">
 
-					<?php if ( ! empty( get_the_post_thumbnail_url() ) ) : ?>
-
-                        <div class="mb-5">
-                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="rounded img-thumbnail" alt="">
+                        <div class="metabox metabox--position-up metabox--with-home-link">
+                            <p>
+                                <a class="metabox__blog-home-link" href="<?php echo esc_url( site_url( '/blog' ) ); ?>">
+                                    <i class="fa fa-home me-1" aria-hidden="true"></i>Blog Home
+                                </a>
+                                <span class="metabox__main">
+                                    Posted by <?php the_author_posts_link(); ?> on <?php the_time( 'd/M/Y' ); ?> in <?php echo get_the_category_list( ', ' ); ?>
+                                </span>
+                            </p>
                         </div>
 
-					<?php endif; ?>
+						<?php if ( has_post_thumbnail() ) : ?>
 
-                    <div class="generic-content">
-						<?php the_content(); ?>
+                            <div class="mb-5">
+								<?php the_post_thumbnail( 'medium_large', array( 'class' => 'img-thumbnail' ) ); ?>
+                            </div>
+
+						<?php endif; ?>
+
+                        <div class="generic-content">
+							<?php the_content(); ?>
+                        </div>
+
                     </div>
-                </div>
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
 
-        </div>
-    </section>
+            </div>
+        </section>
+
+    </main>
 
 <?php get_footer(); ?>

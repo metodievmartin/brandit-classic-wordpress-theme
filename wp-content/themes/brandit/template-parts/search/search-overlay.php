9032958f8f50a26a34cba1b9c3ad266e854206e0
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-09 18:42:17 +0200

    theme: replace hardcoded string with translation functions

--- wp-content/themes/brandit/template-parts/search/search-overlay.php
+++ wp-content/themes/brandit/template-parts/search/search-overlay.php
@@ -2,7 +2,8 @@
     <div class="search-overlay__top">
         <div class="container">
             <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-            <input id="search-term" class="search-term" type="text" placeholder="What are you looking for?"
+            <input id="search-term" class="search-term" type="text"
+                   placeholder="<?php esc_html_e( 'What are you looking for?', 'brandit' ) ?>"
                    autocomplete="off">
             <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
         </div>

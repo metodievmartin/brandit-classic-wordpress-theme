1386beefba3871348af73a556f896502102ee519
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-10 15:33:07 +0200

    theme: move and rename file

--- wp-content/themes/brandit/functions.php
+++ wp-content/themes/brandit/functions.php
@@ -2,7 +2,7 @@
 
 // Load services cpt functionality
 require_once get_template_directory() . '/includes/utils/utility-functions.php';
-require_once get_template_directory() . '/includes/custom-post-types/services.php';
+require_once get_template_directory() . '/includes/sections/services-section.php';
 require_once get_template_directory() . '/includes/walkers/class-bootstrap-nav-walker.php';
 
 function brandit_load_assets() {
@@ -39,6 +39,10 @@ add_action( 'wp_enqueue_scripts', 'brandit_load_assets' );
 function brandit_add_support() {
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
+
+	add_image_size( 'hero-banner', 1200, 675, true );
+	add_image_size( 'hero-banner-portrait', 900, 1120, true );
+	add_image_size( 'page-banner', 1500, 350, true );
 
 	register_nav_menus( array(
 		'header-menu'            => __( 'Header Menu', 'brandit' ),

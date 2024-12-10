fcee0a19e571262f210354d6b744bbe78250512c
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-10 14:13:23 +0200

    theme: add 'main' wrapper tag to the page

--- wp-content/themes/brandit/taxonomy-service_category.php
+++ wp-content/themes/brandit/taxonomy-service_category.php
@@ -19,10 +19,14 @@ $cat_filters_args = array(
 );
 ?>
 
-<?php get_template_part( 'template-parts/banners/page-banner', null, $page_banner_args ); ?>
+    <main class="service-category-page">
 
-<?php get_template_part( 'template-parts/services/service-category-filter-section', null, $cat_filters_args ); ?>
+		<?php get_template_part( 'template-parts/banners/page-banner', null, $page_banner_args ); ?>
 
-<?php get_template_part( 'template-parts/services/service-items-listing-section' ); ?>
+		<?php get_template_part( 'template-parts/services/service-category-filter-section', null, $cat_filters_args ); ?>
+
+		<?php get_template_part( 'template-parts/services/service-items-listing-section' ); ?>
+
+    </main>
 
 <?php get_footer(); ?>

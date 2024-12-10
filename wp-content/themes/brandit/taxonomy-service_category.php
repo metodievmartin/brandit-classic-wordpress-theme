6ac7fafce64e46ad998583e93c9127429d42936d
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-10 14:10:40 +0200

    theme: refactor custom service-category page to use template parts

--- wp-content/themes/brandit/taxonomy-service_category.php
+++ wp-content/themes/brandit/taxonomy-service_category.php
@@ -2,38 +2,27 @@
 
 <?php
 // Get the current term object
-$current_term      = get_queried_object();
-$current_term_slug = $current_term ? $current_term->slug : '';
+$current_term             = get_queried_object();
+$current_term_slug        = $current_term->slug ?? '';
+$current_term_description = $current_term->description ?? '';
+
+// Page Banner
 $page_title_prefix = __( 'Service Category: ', 'brandit' );
 $page_banner_args  = array(
-	'page_title' => single_term_title( $page_title_prefix, false )
+	'page_title'    => single_term_title( $page_title_prefix, false ),
+	'page_subtitle' => $current_term_description
+);
+
+// Category Filter Section
+$cat_filters_args = array(
+	'current_term_slug' => $current_term_slug
 );
 ?>
 
 <?php get_template_part( 'template-parts/banners/page-banner', null, $page_banner_args ); ?>
 
-    <section class="service-category-filters page-section container-xxl">
-		<?php display_service_category_buttons( $current_term_slug ); ?>
-    </section>
+<?php get_template_part( 'template-parts/services/service-category-filter-section', null, $cat_filters_args ); ?>
 
-    <section class="services-section page-section container-xxl">
-        <div class="row justify-content-center align-items-center g-4">
-
-			<?php while ( have_posts() ) : the_post();
-
-				$card_args = array(
-					'title'         => get_the_title(),
-					'short_summary' => get_the_excerpt(),
-					'bg_image_url'  => get_the_post_thumbnail_url(),
-					'service_url'   => get_the_permalink(),
-				);
-
-				// Call the template part for each service
-				get_template_part( 'template-parts/services/service-item', null, $card_args );
-
-			endwhile; ?>
-
-        </div>
-    </section>
+<?php get_template_part( 'template-parts/services/service-items-listing-section' ); ?>
 
 <?php get_footer(); ?>

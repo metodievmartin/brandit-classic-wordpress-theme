364142fd9ffe15b4f2588ce6e3e21b5794c8ea02
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-10 15:27:07 +0200

    theme: add button classes as a dynamic property to the button-link template part

--- wp-content/themes/brandit/template-parts/base/button-link.php
+++ wp-content/themes/brandit/template-parts/base/button-link.php
@@ -1,12 +1,13 @@
 <?php
-$button_title = $args['button_title'] ?? 'Title';
-$button_url   = $args['button_link'] ?? '#';
-// TODO: accept classes and other options to add flexibility
+$button_title   = $args['button_title'] ?? 'Title';
+$button_url     = $args['button_link'] ?? '#';
+$button_classes = $args['button_classes'] ?? '';
+// TODO: other options to add flexibility
 ?>
 
 <div class="text-center">
     <a href="<?php echo esc_url( $button_url ); ?>"
-       class="btn btn-outline-primary text-dark">
+       class="btn <?php echo esc_attr( $button_classes ); ?>">
 		<?php echo esc_html( $button_title ); ?>
     </a>
 </div>

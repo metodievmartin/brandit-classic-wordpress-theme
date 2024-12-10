d9361adfbd37952e0d68ebd13e8d3cab565b4caf
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-10 14:09:24 +0200

    theme: add new dynamic property accepting classes to service-item template part

--- wp-content/themes/brandit/template-parts/services/service-item.php
+++ wp-content/themes/brandit/template-parts/services/service-item.php
@@ -1,15 +1,16 @@
 <?php
-$title          = $args['title'] ?? 'Service Title';
-$short_summary  = $args['short_summary'] ?? 'Service Short Summary ';
-$bg_image_url   = $args['bg_image_url'] ?? '';
-$icon_image_url = $args['icon_image_url'] ?? '';
-$service_url    = $args['service_url'] ?? '#';
-$num_of_words   = $args['num_of_words'] ?? 20;
-$cta_text       = $args['$cta_text'] ?? 'Read More';
+$title                = $args['title'] ?? 'Service Title';
+$short_summary        = $args['short_summary'] ?? 'Service Short Summary ';
+$bg_image_url         = $args['bg_image_url'] ?? '';
+$icon_image_url       = $args['icon_image_url'] ?? '';
+$service_url          = $args['service_url'] ?? '#';
+$num_of_words         = $args['num_of_words'] ?? 20;
+$cta_text             = $args['$cta_text'] ?? 'Read More';
+$service_item_classes = $args['service_item_classes'] ?? '';
 ?>
 
 <div class="col-lg-8 col-md-10">
-    <div class="service-item row h-100">
+    <div class="service-item row h-100 <?php echo esc_attr( $service_item_classes ); ?>">
 
 		<?php if ( ! empty( $bg_image_url ) ) : ?>
             <div class="p-0 col-12 col-md-5 service-image">
